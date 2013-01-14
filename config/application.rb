d058719cf77ca406b40b9b276c4a24ab48a8eab1
Shuddhashil Ray <rayshuddhashil@gmail.com>
2013-01-15 03:46:30 +0530

    New Prod System for Heroku Branch Init

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,6 @@ module Socialbeam
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
@@ -61,7 +60,7 @@ module Socialbeam
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.initialize_on_precompile=false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
