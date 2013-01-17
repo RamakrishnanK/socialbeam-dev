d812d78f3e512e9f30e9b459fdb2a23f5a43e4b8
Shuddhashil Ray <shuddhashil.ray@infibeam.net>
2013-01-17 14:01:02 +0530

    Working Capistrano Deployment Configs with Migrations in AWS

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,17 @@
-set :rvm_ruby_string, '1.9.2@rails311.1.9.2'
-set :rvm_type, :user
+#Deployment Server is running on the following most important platforms
+#RVM (rvm 1.17.9)
+#Rails(3.2.11)
+#Ruby (ruby 1.9.2p320)
+#Capistrano (capistrano (2.14.1)\
+#RVM-Capistrano (rvm-capistrano (1.2.7))
+#Phusion Passenger version 3.0.19
+#Apache/2.2.20
+#For more details on RVM -Capistrano integration see here https://rvm.io/integration/capistrano/
+ #Make sure to install 'rvm-capistrano' in deployment server before 
 
+set :rvm_ruby_string, '1.9.2'  #global in deployment server
+set :rvm_type, :system #system-wide RVM installation in server
+require "rvm/capistrano"
 set :application, "socialbeam"
 set :scm, :git
 set :repository, "git@github.com:raycoding/socialbeam-dev.git"
